9313ff7aee56a9ce15d476f2d4893004171bdb44
workbill1994-droid <workbill1994@gmail.com>
2026-07-06 14:16:00 +0800

    Update sw.jsRefresh app icon cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "land-calc-v3";
+const CACHE_NAME = "land-calc-v4";
 const APP_FILES = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (event) => {
